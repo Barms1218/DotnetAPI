0f5e28c4c04a11f57168da66de25ba4b978c90a2
Branden <branden22894@gmail.com>
2024-08-05 15:26:35 -0600

    Renamed User dto

--- Dtos/UserToAddDto.cs
+++ Dtos/UserToAddDto.cs
@@ -1,6 +1,6 @@
 namespace DotNetAPI.Dtos;
 
-public partial class UserDto
+public partial class UserToAddDto
 {
     public string FirstName { get; set; } = "";
     public string LastName { get; set; } = "";
--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b0530ad524206fbf9ee18d440f0306e0ce4f5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51eec99c8388820aaa90a674873acd3f388543ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
